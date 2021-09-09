b68ed9eee5c5ee9e6f605400e9156f56567d4d2a
Flevertyapp <Flevertyap@gmail.com>
2021-09-10 01:06:50 +0300

    add browser calling. It was too hard)

--- app/src/main/java/ru/example/implictintent/MainActivity.java
+++ app/src/main/java/ru/example/implictintent/MainActivity.java
@@ -2,7 +2,12 @@ package ru.example.implictintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText site = findViewById(R.id.editText);
+        Button go = findViewById(R.id.button);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = site.getText().toString();
+                Uri uri = Uri.parse(url);
+                Intent browser = new Intent(Intent.ACTION_VIEW,uri);//интент с действием "показать"
+                startActivity(browser);
+            }
+        });
     }
 }
